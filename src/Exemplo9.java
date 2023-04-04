6b74f6703bc077758af5e0fa518e6643f01e0fe8
Thiago Pereira Da Silva <thiago.silva@venturus.org.br>
2023-04-04 14:52:12 -0300

    Meus arquivos de Estoque

---
+++ src/Exemplo9.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Exemplo9 {
+    public static void main(String[] args) {
+        Map<Produto, Double> estoque = new HashMap<>();
+        Produto p1 = new Produto("Tv", 900.0);
+        Produto p2 = new Produto("Notebook", 1200.0);
+        Produto p3 = new Produto("Tablet", 400.0);
+        estoque.put(p1, 10000.0);
+        estoque.put(p2, 20000.0);
+        estoque.put(p3, 15000.0);
+        Produto ps = new Produto("Tv", 900.0);
+        System.out.println("Contem chave 'ps': " + estoque.containsKey(ps));
+    }
+}
